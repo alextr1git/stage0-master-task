fbfb52b438b1cda4bc56c3656f78588eab006d2d
alextr1git <dasprooa3@gmail.com>
2023-06-15 12:43:34 +0300

    half-completed

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -1,13 +1,23 @@
 package com.epam.loops;
 
 public class HalfPyramid {
-
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++){
+
+            for (int j = cathetusLength; j >= 1; j--){
+                if (j > i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
 
     }
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
     }
-
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,32 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++){
 
+            for (int j = cathetusLength; j > 1; j--){
+                if (j > i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(j);
+                }
+            }
+
+            for (int j = 1; j <= cathetusLength; j++){
+                if (j > i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
